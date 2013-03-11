ccf34e97fad8b6c82e280714b0a921c141a2b653
Ash Mady <anmady@ung.edu>
2013-03-11 16:11:14 -0400

    New layout created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyNewApp::Application.config.secret_token = '71b505b3a28417606bb2c181d42af96cab48de7ff86993d387d1a35252acfbf500e3870b79a83d28b561bd4b2145903f4f1d602b9f3e1a4d5619899b34ac3de9'
